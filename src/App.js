c530b808d9ce274931952cb686f7efdf31eca8e3
MadhuGorava <goravam143@gmail.com>
2023-01-02 13:03:47 +0000

    LoginForm First Commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import LoginForm from './components/LoginForm'
+import './App.css'
+
+const sortByOptions = [
+  {
+    id: 0,
+    displayText: 'Highest',
+    value: 'Highest',
+  },
+  {
+    id: 2,
+    displayText: 'Lowest',
+    value: 'Lowest',
+  },
+]
+
+const App = () => <LoginForm />
+
+export default App
